bb1b918bdc20117e51272cf6a20f9d1af7ab3dcc
LUA TRAN THI <1821050681@student.humg.edu.vn>
2021-09-27 00:10:59 +0700

    first commit

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoMvc.Models;
+namespace DemoMvc.Data
+{
+     public class ApplicationContext : DbContext
+    {
+        public ApplicationContext (DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoMvc.Models.Movie> Movie { get; set; }
+    }
+
+}
+
+   
